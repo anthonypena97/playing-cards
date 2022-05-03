c8bc64336ecfe11f8e14492eb7a125895181d40c
Anthony Pena <apena5@alumni.jh.edu>
2022-05-03 12:39:55 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var card string = "Ace of Spades"
+	// card := "Ace of Spades" // variable with type String
+	// card = "Five of Diamonds"
+
+	card := newCard()
+
+	fmt.Println(card)
+
+}
+
+func newCard() string {
+
+	return "Five of Diamonds"
+
+}
