dbfc75fb5b1273ce4b6c6e825b81b78d23d515da
Anthony Pena <apena5@alumni.jh.edu>
2022-05-03 18:12:37 -0400

    created deck type with print method

--- deck.go
+++ deck.go
@@ -1,5 +1,15 @@
 package main
 
-func deck() {
+import "fmt"
+
+// Create a new type of 'deck'
+// which is a slice of string
+type deck []string
+
+func (d deck) print() {
+
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
 
 }
--- main.go
+++ main.go
@@ -1,26 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	// var card string = "Ace of Spades"
-	// card := "Ace of Spades" // variable with type String
-	// card = "Five of Diamonds"
-
-	// card := newCard()
-
-	// fmt.Println(card)
-
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
-	// iterating over a closed set
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-
-	// fmt.Println(cards)
+	cards.print()
 
 }
 
