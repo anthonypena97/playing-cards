68ed5d241ca3371cd4c30358a7f3be51c3fb2b40
Anthony Pena <apena5@alumni.jh.edu>
2022-05-04 12:47:12 -0400

    new deck function created

--- deck.go
+++ deck.go
@@ -6,6 +6,24 @@ import "fmt"
 // which is a slice of string
 type deck []string
 
+func newDeck() deck {
+
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	// 'i' and 'j' are replaced with underscores, '_' , to let go know we don't need these variables
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+
+}
+
 // function with a receiver declared as 'd'
 // by convention - the receiver is commonly decalred as one or two letters that match to the type
 func (d deck) print() {
